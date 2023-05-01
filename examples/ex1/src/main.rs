c04c8371c38a725f66cde8746abc0baedb87046a
codeure24 <85784092+antoineB24@users.noreply.github.com>
2023-05-01 09:58:30 +0200

    change main.rs

--- examples/ex1/src/main.rs
+++ examples/ex1/src/main.rs
@@ -3,47 +3,7 @@ use popper::execute;
 
 fn main() {
     println!("{:?}", execute(r#"
-    use "io.pop" as io
-    use "itertool.pop" as itertool
-
-
-
-    fun syracus(n) {
-        if n == 1 {
-            n
-        } else if n % 2 == 0 {
-                syracus(n / 2)
-        } else {
-            syracus(3 * n + 1)
-        }
-
-    }
-
-    fun fib(n) {
-        if n < 2 {
-            n
-        } else {
-            fib(n - 1) + fib(n - 2)
-        }
-    }
-
-    io::println(
-        fib(10)
-    )
-    io::println(
-        syracus(10)
-    )
-    io::println(
-        itertool::map(fib, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-    )
-
-    io::println(
-        itertool::map(syracus, 1:10000000)
-    )
-
-    io::println(
-        0:10
-    )
+    
 
     "#, None));
 }
