e8473c5bda430d9b287547f0d81aca30b774aa4e
NightProg <tonio.barbier@gmail.com>
2024-02-26 18:08:20 +0100

    remove file

--- shell.nix
+++
@@ -1,9 +0,0 @@
-let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
-in
-
-pkgs.mkShell {
-  buildInputs = [ pkgs.cargo pkgs.rustc pkgs.llvmPackages_17.stdenv ];
-
-}
