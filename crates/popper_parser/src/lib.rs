261e17d5126dc2a3a393ada5f0a3f2886fe8ad3f
NightProg <tonio.barbier@gmail.com>
2024-03-03 13:28:59 +0100

    Run `cargo fmt` on lib popper_parser

--- crates/popper_parser/src/lib.rs
+++ crates/popper_parser/src/lib.rs
@@ -1,7 +1,5 @@
-
-pub mod parser;
 pub mod error;
+pub mod parser;
 mod test;
 
 pub use ariadne::Source;
-
--- crates/popper_parser/src/parser.rs
+++ crates/popper_parser/src/parser.rs
@@ -1,15 +1,11 @@
 use lalrpop_util::lalrpop_mod;
 
 use crate::error::{ParserError, ParserErrorType};
+use popper_ast::Statement;
 use popper_error::generate_color;
 use popper_error::Error;
-use popper_ast::Statement;
-
-
-
 
 lalrpop_mod!(pub popper); // lalrpop gen code
-
 
 pub fn parse<'a>(input: &'a str, file: &'a str) -> Option<Vec<Statement>> {
     let res = popper::FileParser::new().parse(input);
@@ -22,11 +18,7 @@ pub fn parse<'a>(input: &'a str, file: &'a str) -> Option<Vec<Statement>> {
                 error: err_type,
                 span: popper_ast::Span::new(0, input.len()),
             };
-            err.report(
-                generate_color(),
-                input,
-                file
-            );
+            err.report(generate_color(), input, file);
             None
         }
     }
